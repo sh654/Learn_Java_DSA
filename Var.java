69e7883b980c2f3a962ac96a3ec4081259692025
Shivam Pravin Upadhyay <shivampdh2105@gmail.com>
2023-07-16 17:20:55 +0530

    loopeg.java

--- Var.java
+++ Var.java
@@ -44,5 +44,62 @@ public class Var {
         byte bb1 = 2;
         int mul = bb * bb1;
         System.out.println(mul); 
+
+
+        //increment operators 
+        a1++; //post-increment
+        System.out.println(a1);
+        
+        ++b1; //pre-increment
+        System.out.println(a1);
+
+        //but here
+        // int r = ++a1;
+        // System.out.println(r); //This works because first increment and then fetch the value of it.
+
+        int r = a1++;
+        System.out.println(r); // Here it doesn't because it first fetch the value then increment it.
+
+        int x1 = 8;
+        int y1 = 7;
+        // boolean res = x1 >= y1; Relational
+
+        double aa = 1.23;
+        double be = 2.22;
+        boolean res = x1>=y1 && aa<=be;
+        System.out.println(res);
+
+        // conditonal statements 
+        int h = 1;
+        if( h > 10){ //if can hold multiple conditions like h>=10 && h<=20 gives false 
+            System.out.println(h);
+        } else{
+            System.out.println("small statement");
+        }
+        
+        int xx = 8;
+        int yy = 7;
+        int zz = 9;
+        String mm = "The greatest is: ";
+        // To get max of them 
+        if(xx>yy && xx>zz){
+            System.out.println(mm+xx);
+        } else if(yy>zz){ // zJust to increase efficiency of code we wont compare yy with zz think about it by looking at above condition 
+            System.out.println(mm + yy);
+        } else{
+            System.out.println(mm + zz);
+        }
+        //Ternary operator ?:
+        int nn = 44;
+        int resnn = 0;
+        /* 
+         * if(n%2==0)
+         *      result = 10;
+         * else 
+         *      result = 20;
+        */
+
+        resnn = nn%2==0 ? 10 : 20;
+        System.out.println(resnn);
     }
 }
