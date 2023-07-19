48977ee5ef6ebb82d5f43ac077b49f0124faa549
Shivam Pravin Upadhyay <shivampdh2105@gmail.com>
2023-07-19 13:54:14 +0530

    methodoverload.java

---
+++ Patternseg/square1.java
@@ -0,0 +1,14 @@
+import java.util.*;
+public class square1 {
+    public static void main(String a[]){
+        int n = 4; //square of row 4
+        int m = 5; // column 5
+        for(int i = 1; i<=n; i++){
+            for(int j=1; j<=m; j++){
+                System.out.print("*"); // to get 5 star in same row 
+            }
+        System.out.println("*"); // to get new rows 
+        }
+
+    }
+}
