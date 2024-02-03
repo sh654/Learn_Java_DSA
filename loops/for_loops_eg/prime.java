13c3fb204a90151ef6f335ebcb12370fbea3fad2
Shivu2105 <shivampdh2105@gmail.com>
2024-02-03 12:50:50 +0530

    prime sol

---
+++ loops/for_loops_eg/prime.java
@@ -0,0 +1,12 @@
+package loops.for_loops_eg;
+
+public class prime {
+    public static void main(String[] args) {
+        int a = 10;
+        for(int i=1; i<=a; i++){
+            if(i%2==0){
+                System.out.println(i);
+            }
+        }
+    }
+}
